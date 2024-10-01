33f100903907538fd80e880fd7cd7cbdcc7b6e12
Syed Konain Nasir <konain611@gmail.com>
2024-10-01 23:30:21 +0500

    .

--- Admin/adminPortal.php
+++ Admin/adminPortal.php
@@ -8,7 +8,9 @@ $pass = '';
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
+    
     die("Connection failed: " . $conn->connect_error);
+
 }
 
 $sql = "SELECT * FROM stu_registration";
