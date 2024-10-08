0e1d91ee3a99c8e4ade096f1bf0f64c91d5cee4b
Syed Konain Nasir <konain611@gmail.com>
2024-10-08 23:36:04 +0500

    Update delete.php

--- Faculty/delete.php
+++ Faculty/delete.php
@@ -30,6 +30,7 @@ if (is_numeric($id) && $id > 0) {
 
     echo "<h1>Delete Faculty Record?</h1>";
     echo "<p style='text-align:center; font-size:35px; color:Yellow;';>Are you sure you want to delete the Faculty record with ID $id?</p>";
+    
     echo "<form action='delete.php' method='post'>";
     echo "<input type='hidden' name='id' value='$id'>";
     echo "<input type='submit' name='confirm' value='Yes, delete'>";
