2a3a6d7e20d500203d375de1c46d69d5d3f07f6b
Syed Konain Nasir <konain611@gmail.com>
2024-10-08 23:41:50 +0500

    Update edit.php

--- Faculty/edit.php
+++ Faculty/edit.php
@@ -19,7 +19,9 @@ if (isset($_GET['id'])) {
 }
 
 if (isset($id) && !empty($id)) {
+    
     $conn = new mysqli($host, $user, $pass, $db);
+
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
